0149a895e486832a0ef336df3b8234b81d6cae85
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-08-26 16:55:37 -0700

    deleted whitespace and added docstrings

--- client.py
+++ client.py
@@ -3,11 +3,16 @@ import uuid
 
 class ChatClient:
     def __init__(self, conn=None, addr=None):
+        """This creates a new user in the chat room. The user is initially given an id of a random uuid, and a nickname of a random
+        number. This nickname can be changed using the change_name class method directly below this method.
+        """
         self.id = str(uuid.uuid4())
         self.nick = 'user_{}'.format(random.random())
         self.conn = conn
         self.addr = addr
 
     def change_name(self, new_name):
+        """This allows the user to change their nickname.
+        """
         self.nick = new_name
 
--- server.py
+++ server.py
@@ -8,9 +8,11 @@ import random
 PORT = random.randint(1000, 5000)
 
 
-
 class ChatServer(threading.Thread):
     def __init__(self, port, host='localhost'):
+        """This initializes the server by setting the port, host, and server attributes of the thread.
+        This also creates the user pool, which is the list of all the users that are currently in the chatroom.
+        """
         super().__init__(daemon=True)
         self.port = port
         self.host = host
@@ -19,79 +21,70 @@ class ChatServer(threading.Thread):
             socket.SOCK_STREAM,
             socket.IPPROTO_TCP,
         )
-
         self.client_pool = []
-
         try:
             self.server.bind((self.host, self.port))
         except socket.error:
             print('Bind failure {}'.format(socket.error))
             sys.exit()
-
         self.server.listen(10)
 
-
     def run_thread(self, client):
+        """This function logs when a user enters the classroom, then runs an inifinite loop that waits for
+        user data to be received, then displays that data.
+            ARGS:
+                The client info for the connecting user.
+        """
         print('{} connected with {}:{}'.format(client.nick, client.addr[0], str(client.addr[1])))
-
         try:
-
             while True:
                 data = client.conn.recv(4096)
                 self.parser(client, data)
-                # if len(data) < 4096:
-                #     break
-
         except (ConnectionResetError, BrokenPipeError, OSError):
             client.conn.close()
 
-        # self.parser(client.id, client.nick, client.conn, message)
-        # for cl in self.client_pool:
-        #     cl.conn.sendall(message.encode())
-
-
-
-
     def parser(self, client, message):
-        if message.decode().startswith('@'):
+        """This is the main message decoder method. It first checks if the message started with a "/", which indicates if its
+        a command. It then goes through all the possible message commands, and executes the proper one if present. If the entered
+        data is not a valid command, and error is returned to the user. If the message does not start with a "/", the text is sent as
+        a global message to the chatroom.
+            ARGS:
+                1. The sending client information
+                2. The sent message from said client
+        """
+        if message.decode().startswith('/'):
             data = message.decode().split(maxsplit=1)
-            if data[0] == '@quit':
+            if data[0] == '/quit':
                 client.conn.sendall(b'You have left the chatroom\n')
                 reply = client.nick.encode() + b' has left the channel\n'
                 [c.client.conn.sendall(reply) for c in self.client_pool if len(self.client_pool)]
                 self.client_pool = [c for c in self.client_pool if c.client.id != client.id]
                 client.conn.close()
-            elif data[0] == '@list':
+            elif data[0] == '/list':
                 user_list = []
                 for c in self.client_pool:
                     user_list.append(str(c.nick))
                 client.conn.sendall(str(user_list).encode() + b'\n')
-            elif data[0] == '@nickname':
+            elif data[0] == '/nickname':
                 new_nick = data[1].replace('\n', '')
                 for c in self.client_pool:
                     if client.id == c.id:
                         c.nick = new_nick
-            elif data[0] == '@dm':
+            elif data[0] == '/dm':
                 user_and_message = data[1].split(maxsplit=1)
                 for c in self.client_pool:
                     if user_and_message[0] == c.nick:
-                        message = client.nick.encode() + b' @dm: ' + user_and_message[1].encode()
+                        message = b'@' + client.nick.encode() + b': ' + user_and_message[1].encode()
                         c.conn.sendto(message, c.addr)
             else:
                 client.conn.sendall(b'Invalid Command, please try again. \n')
         else:
             reply = client.nick.encode() + b': ' + message
-            # for cl in self.client_pool:
-            #       if len(self.client_pool) > 0:
-                    # cl.conn.sendall(reply)
-            #     cl.conn.sendall(reply)
-            # The line below is the exact same thing
             [c.conn.sendall(reply) for c in self.client_pool if len(self.client_pool)]
 
-
-
-
     def run(self):
+        """This method uses an infinite loop to keep the chatroom open if called.
+        """
         print('Server is running on {}'.format(self.port))
         while True:
             conn, addr = self.server.accept()
